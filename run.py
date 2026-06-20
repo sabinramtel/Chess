93304243dbd397a6e16e237920fe22330fcdaffd
unknown <gramtelsabeen96@gmail.com>
2026-06-20 14:16:28 +0545

    Allow werkzeug server in production

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "True").lower() == "true"
-    socketio.run(app, debug=debug, host="0.0.0.0", port=port)
+    socketio.run(app, debug=debug, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
